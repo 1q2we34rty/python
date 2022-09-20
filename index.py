07366c9152587501483498dc141460fce8e6fba7
1q2we34rty <ico443715@gmail.com>
2022-09-20 17:45:49 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,16 @@
+import main
+
+main.add(20, 40)
+main.sub(10, 5)
+print(main.it)
+
+from main import add, sub, it
+add(10, 100)
+sub(10, 1)
+print(it)
+
+from main import *
+add(10, 40)
+print(it)
+sub(10, 20)
+students("Kurmanbek", "Adilet", "Sardor")
